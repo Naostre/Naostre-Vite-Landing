030ffa85e50e7b04a661400f8a03ecbf66acfb0d
Sacramentix <benji.u1225u@gmail.com>
2021-11-08 23:56:12 +0100

    Vite + ts + unocss + aos

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.scss'
+import 'uno.css'
+import aos from "aos"
+
+aos.init({
+  duration: 800
+});
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import Unocss from 'unocss/vite'
+
+import { presetUno, presetAttributify } from 'unocss'
+
+export default {
+  plugins: [
+    Unocss({
+      theme: {
+        colors: {
+          main: "#ffffe5",
+          alt: "#A9F1DF",
+          theme: "#1EAE98",
+          contrast: "#233E8B",
+          accent: "#D36582",
+        }
+      },
+      presets: [
+        presetAttributify({ strict: true }),
+        presetUno(),
+        // ...custom presets
+      ],
+      rules: [
+        ['dshadow', { "box-shadow": "0 0 10px 0px #2227"}]
+      ]
+     })
+  ]
+}
